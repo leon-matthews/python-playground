f899d086fa0193c8622d4235173d404623fe55c1
Leon Matthews <leon@digitaladvisor.nz>
2024-04-05 10:13:32 +1300

    Review documentation

--- text/readline_example.py
+++ text/readline_example.py
@@ -8,7 +8,7 @@ import logging
 import readline
 
 
-# We can't print to console to debug, so we're logging instead
+# Readline takes over the console, so lets debug to a file instead.
 logging.basicConfig(
     format='%(message)s',
     filename='readline_example.log',
@@ -26,19 +26,54 @@ keywords = [
 
 
 def input_loop(completer: Completer) -> None:
+    """
+    Sample input loop.
+
+    We're not doing anything except adding new words to the completer.
+
+    Note just importing the readline module changes the behaviour of
+    the  `input()` function.
+
+    Args:
+        completer:
+            Completer object, just so we can play with adding new words.
+
+    Returns:
+        None
+    """
     while True:
         line = input('input> ')
         if line.casefold() in ('quit', 'exit'):
             break
         print(f">>> {line!r}")
 
+        # Add new words to completer, just because.
         for word in line.split():
             completer.add(word)
 
 
 class Completer:
     """
-    Class-based completion function.
+    Class-based completion function for readline module.
+
+    Use to provide custom tab-completion of words while using a readline-
+    powered prompt. Words to complete can be provided both during creation
+    and later during operation.
+
+    Install the completion function by calling readline's module-level
+    functions::
+
+        completer = Completer(['list', 'of', 'keywords')
+        readline.set_completer(completer.complete)
+        readline.parse_and_bind('tab: complete')
+
+    Extra words to complete can be added at any time::
+
+        completer.add('print')
+
+    There's loads of room for optimisation (a radix trie?), and some interesting
+    edge-cases (matches cache) to play around with, but I must resist the
+    urge to over-engineer!
     """
     def __init__(self, words: list[str]|None) -> None:
         """
@@ -48,20 +83,26 @@ class Completer:
             words:
                 Initial set of words for completion list.
         """
+        # Keep completion list sorted.
         self.words = [] if words is None else words
         self.words.sort()
 
-        # Current set of matches
+        # Current set of matches. Not thread-safe.
         self._matches = []
 
     def add(self, word: str) -> None:
         """
         Add word to list of completions.
+
+        Args:
+            word:
+
         """
         # Ignore repeats
         if word in self.words:
             return
 
+        # Luckily, Python sorts already sorted lists very fast indeed.
         self.words.append(word)
         self.words.sort()
 
@@ -81,7 +122,7 @@ class Completer:
                 match.
 
         Returns:
-            Word
+            Word to complete, none if matches exhausted.
         """
         # Find possible matches on first call
         if state == 0:
