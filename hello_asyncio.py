850ffde3fd265d1dd8d81cbce3d79b90d824818d
Leon Matthews <leon@digitaladvisor.nz>
2024-04-20 11:21:40 +1200

    Hello world for asyncio module

---
+++ hello_asyncio.py
@@ -0,0 +1,18 @@
+
+import asyncio
+import time
+
+
+async def count():
+    print("One")
+    await asyncio.sleep(1)
+    print("Two")
+
+
+async def main():
+    counters = (count() for _ in range(10))
+    await asyncio.gather(*counters)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
