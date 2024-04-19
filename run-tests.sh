8509e62b2b766ddd2c34bef45d7d13d4f2e04242
Leon Matthews <leon@digitaladvisor.nz>
2024-04-20 11:17:46 +1200

    Run all tests and linters all at once

--- run-tests.sh
+++ run-tests.sh
@@ -7,4 +7,15 @@ set -o nounset
 set -o errexit
 set +o xtrace
 
-python3 -m unittest --catch --failfast --verbose
+
+# Abort run if Ruff picks up any errors
+ruff check --output-format grouped
+
+
+# Run unitests, show files without 100% branch coverage
+coverage run --branch -m unittest
+coverage report --show-missing --skip-covered
+coverage erase
+
+# Check optional static typing
+mypy --sqlite-cache .
