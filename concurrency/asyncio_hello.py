4889e63590c2015eef020566de3ec9e379285eb5
Leon Matthews <leon@digitaladvisor.nz>
2024-06-19 11:57:14 +1200

    Document asyncio example

---
+++ concurrency/asyncio_hello.py
@@ -0,0 +1,24 @@
+"""
+The 'hello world' of asyncio coroutines and tasks.
+"""
+
+import asyncio
+from pprint import pprint as pp
+
+
+async def count() -> None:
+    print("One")
+    await asyncio.sleep(1)
+    print("Two")
+
+
+async def main() -> None:
+    # Create several coroutines by calling `count()`
+    counters = (count() for _ in range(10))
+
+    # Calling `gather()` creates running tasks then waits for them to finish
+    await asyncio.gather(*counters)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
