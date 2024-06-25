f908a9f0088f00c3e384ba4276fd4f67c2d1b1b0
Leon Matthews <leon@digitaladvisor.nz>
2024-06-26 10:34:58 +1200

    Play around with fast exponentiation algorithm

---
+++ mathematics/fast_exponentiation.py
@@ -0,0 +1,23 @@
+
+import math
+
+
+def power(base: int, exponent: int) -> int:
+    """
+    Recursively raise base to the power of the given exponent.
+    """
+    print(f"power({base}, {exponent})")
+    if exponent == 0:
+        return 1
+
+    x = power(base, exponent // 2)
+
+    if (exponent % 2) == 0:
+        return x**2
+    else:
+        return base * x**2
+
+
+if __name__ == '__main__':
+    print(power(2, 1000))
+    print(2**1000)
