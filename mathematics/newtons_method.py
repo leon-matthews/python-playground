2b89fa2addb142fa008ca9693cbc605c2562dc78
Leon Matthews <leon@digitaladvisor.nz>
2024-08-20 10:37:08 +1200

    Play around with Newton's method to find square roots

---
+++ mathematics/newtons_method.py
@@ -0,0 +1,24 @@
+
+n = number of interest
+x = any guess
+
+root = 0.5 * (x + (n/x))
+
+===
+
+== Square root of 2 (1.4142135623730951)
+
+1) Try guess of 1.5
+
+root = 0.5 * (1.5 + (2/1.5))
+root = 0.5 * (1.5 + 1.333333)
+root = 0.5 * 2.83333333
+root = 1.416666666
+
+
+2) Now try another iteration using new guess
+
+root = 0.5 * (1.416666666 + (2/1.416666666))
+root = 0.5 * (1.416666666 + 1.411764705882353)
+root = 0.5 * 2.8284313725467127
+root = 1.4142156862733564
