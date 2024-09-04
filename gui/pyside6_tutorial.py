ed0c41bbf2a8d039c18f537cf73977a2172f5d6d
Leon Matthews <leon@digitaladvisor.nz>
2024-09-04 21:39:16 +1200

    Python's Qt bindings are getting better and better

---
+++ gui/pyside6_tutorial.py
@@ -0,0 +1,28 @@
+"""
+
+Installation:
+
+    $ pip install pyside6
+    $ sudo apt install libxcb-cursor0
+
+See:
+    https://doc.qt.io/qtforpython-6/tutorials/index.html
+"""
+import sys
+from PySide6.QtCore import Qt, Slot
+from PySide6.QtWidgets import QApplication, QPushButton
+
+
+@Slot()
+def say_hello():
+    print("Thanks!")
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    button = QPushButton("Push Me")
+    button.clicked.connect(say_hello)
+    button.show()
+
+    sys.exit(app.exec())
